930f493727db373863372fdd9ac094e7039a9e05
Eric Walton <ecw8180@gmail.com>
2014-08-02 15:54:15 -0400

    Fixed form validation affecting logout

--- Diet-and-Exercise-Application/Model1.Designer.cs
+++ Diet-and-Exercise-Application/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Mackenzie\Documents\GitHub\Diet-and-Exercise-Application\Diet-and-Exercise-Application\Model1.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Eric Walton\Documents\Georgian College\Courses\Enterprise Computing\Diet-and-Exercise-Application\Diet-and-Exercise-Application\Model1.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
